6b33322590d42fc79665141e913f7c57458a29b1
Monish Khambalkar <monishkhambalkar@gmail.com>
2024-04-28 16:02:30 +0530

    add register validation

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 import express, { NextFunction, Request, Response } from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandlers";
+import userRouter from "./user/userRouter";
+import bodyParser from "body-parser";
 
 const app = express();
+
+// app.use(express.json());
+app.use(bodyParser.json());
 
 // Routes
 app.get("/", (req, res, next) => {
   res.json({ message: "welcome to master api session" });
 });
+
+app.use("/api/users", userRouter);
 
 app.use(globalErrorHandler);
 
