fabac08c3c7bcdc325beda3164aa1443717454f9
Monish Khambalkar <monishkhambalkar@gmail.com>
2024-04-28 20:42:35 +0530

    change create user status code

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -50,10 +50,10 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     });
 
     //response
-    res.json({ accessToken: token });
+    res.status(201).json({ accessToken: token });
   } catch (error) {
     return next(createHttpError(500, "Error while signing jwt token"));
   }
 };
 
-export { createUser };
+export { createUser, loginUser };
