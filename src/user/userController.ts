34e6a37e2bb471dcc4200a6b43165de7010efd55
Monish Khambalkar <monishkhambalkar@gmail.com>
2024-04-28 21:04:31 +0530

    add login endpoint

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -47,6 +47,7 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     //Token generation JWT
     const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
       expiresIn: "7d",
+      algorithm: "HS256",
     });
 
     //response
@@ -54,6 +55,34 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   } catch (error) {
     return next(createHttpError(500, "Error while signing jwt token"));
   }
+};
+
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(createHttpError(400, "All fields are required"));
+  }
+
+  const user = await userModel.findOne({ email });
+
+  if (!user) {
+    return next(createHttpError(400, "User not found"));
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return next(createHttpError(400, "username or password is incorrect"));
+  }
+  //handle errors
+  //create accession
+  const token = sign({ sub: user._id }, config.jwtSecret as string, {
+    expiresIn: "7d",
+    algorithm: "HS256",
+  });
+
+  res.json({ accessToken: token });
 };
 
 export { createUser, loginUser };
