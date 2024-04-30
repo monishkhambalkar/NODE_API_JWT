d8af80485ba6274ab4c1afdf944851926abdc4af
Monish Khambalkar <monishkhambalkar@gmail.com>
2024-04-30 16:52:04 +0530

    add author id

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -9,6 +9,7 @@ import fs from "node:fs";
 import createHttpError from "http-errors";
 
 import bookModel from "./bookModel";
+import { AuthRequest } from "../middlewares/authenticate";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
   const { title, genre } = req.body;
@@ -55,18 +56,12 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
       }
     );
 
-    console.log("bookFileUploadResult", bookFileUploadResult);
-
-    console.log("uploadResult", uploadResult);
-
-    // @ts-ignore
-
-    console.log("user id", req.userId);
+    const _req = req as AuthRequest;
 
     const newBook = await bookModel.create({
       title,
       genre,
-      author: "662e4272a9cfe81a698de3e5",
+      author: _req.userId,
       coverImage: uploadResult.secure_url,
       file: bookFileUploadResult.secure_url,
     });
